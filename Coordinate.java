936ae1cab6d883a8a831d0bc28549df05dc0e65b
Shubhamk023 <133940492+Shubhamk023@users.noreply.github.com>
2023-05-18 10:21:17 +0530

    Commit message for assignment 1

---
+++ Coordinate.java
@@ -0,0 +1,12 @@
+public class Coordinate{
+	 static void ordinate(int x, int y){
+	System.out.println("["+x+","+y+"]");
+	
+	}
+	public static void main(String[]args)
+	{
+	int x = 5;
+	int y = 6;
+    ordinate(x,y);
+	}
+	}
